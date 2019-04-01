fb0d151180711fceca5d775c2035d7a82cbb3113
Trevor Ramey <trevor.ramey@ttu.edu>
2019-04-01 17:54:29 -0500

    Store Strings in an object and made a method to read input data
    Store Strings in an object for easier DOM manipulation
    
    By moving the DOM strings out of the querySelector, it will make for an easier transiction if the UI is ever updated.
    
     - Fixed typo in Controller module

--- app.js
+++ app.js
@@ -11,6 +11,27 @@
 // UI CONTROLLER
  var UIController = (function(){
 
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+
+    };
+
+    return{
+        getInput: function(){
+            return{ 
+                type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
+                descripttion:document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value,
+            };
+        },
+
+        getDOMstrings: function(){
+            return DOMstrings;
+        }
+    };
 
 
 
@@ -19,8 +40,13 @@
 // GLOBAL APP CONTROLLER
  var controller = (function(budgetCtrl,UICtrl){
 
-    var ctrlAddItem =(){
+    var DOM = UICtrl.getDOMstrings();
+
+    var ctrlAddItem =function() {
+
       // get the filed input data
+      var input = UICtrl.getInput();
+      console.log(input);
 
         // add the item to the budget controller
 
@@ -31,7 +57,7 @@
         // display the budget on the UI  
     };
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress',function(event){
         if (event.keyCode === 13 || event.which === 13){
